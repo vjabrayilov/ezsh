2d0003ad7df920d47b18c573fc478e42e77a1358
vjabrayilov <vjabrayilov@cs.columbia.edu>
2024-05-22 20:23:01 -0400

    fix p10k and vim Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- ezshrc.zsh
+++ ezshrc.zsh
@@ -14,21 +14,22 @@ POWERLEVEL9K_MODE='nerdfont-complete'
 
 ZSH_THEME="powerlevel10k/powerlevel10k"
 
-POWERLEVEL9K_OS_ICON_BACKGROUND="white"
-POWERLEVEL9K_OS_ICON_FOREGROUND="blue"
-POWERLEVEL9K_DIR_HOME_FOREGROUND="white"
-POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="white"
-POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="white"
-
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time background_jobs ram load rvm time)
-
-# more prompt elements that are suggested
-# (public_ip docker_machine pyenv nvm)          https://github.com/bhilburn/powerlevel9k#prompt-customization
-# Note: using public_ip is cool but when connection is down prompt waits for 10-20 seconds
-
-POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(ssh os_icon context dir vcs)
-
-POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+source $HOME/.p10k.zsh
+#POWERLEVEL9K_OS_ICON_BACKGROUND="white"
+#POWERLEVEL9K_OS_ICON_FOREGROUND="blue"
+#POWERLEVEL9K_DIR_HOME_FOREGROUND="white"
+#POWERLEVEL9K_DIR_HOME_SUBFOLDER_FOREGROUND="white"
+#POWERLEVEL9K_DIR_DEFAULT_FOREGROUND="white"
+#
+#POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status command_execution_time background_jobs ram load rvm time)
+#
+## more prompt elements that are suggested
+## (public_ip docker_machine pyenv nvm)          https://github.com/bhilburn/powerlevel9k#prompt-customization
+## Note: using public_ip is cool but when connection is down prompt waits for 10-20 seconds
+#
+#POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(ssh os_icon context dir vcs)
+#
+#POWERLEVEL9K_PROMPT_ON_NEWLINE=true
 
 
 # Uncomment the following line to use case-sensitive completion.
@@ -88,7 +89,7 @@ plugins=(
     # httpie
      git
      python
-     vim
+     vi-mode
     # docker
     # lol
     # pip
