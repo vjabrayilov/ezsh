266923ef475c0be8a6efad902811651a7cf3b991
vjabrayilov <vjabrayilov@cs.columbia.edu>
2025-03-05 17:39:56 -0500

    nit Signed-off-by: vjabrayilov <vjabrayilov@cs.columbia.edu>

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 PURPLE='\033[0;35m'
+NC='\033[0m' # No Color (reset)
 DATE=$(date +"%Y-%m-%d")
 
 for arg in "$@"
@@ -145,7 +146,7 @@ function install_k {
     if [ -d ~/.config/ezsh/oh-my-zsh/custom/plugins/k ]; then
         cd ~/.config/ezsh/oh-my-zsh/custom/plugins/k && git pull
     else
-        git clone --depth 1 https://github.com/supercrabtree/k ~/.config/ezsh/oh-my-zsh/custom/plugins/k > /dev/nul 2>&1
+        git clone --depth 1 https://github.com/supercrabtree/k ~/.config/ezsh/oh-my-zsh/custom/plugins/k > /dev/null 2>&1
     fi
 }
 
